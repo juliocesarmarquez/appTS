79fc05ccf39794ede5373f15fa9b04fa1c4bd546
juliocesarmarquez <juliocesarmarquez88@gmail.com>
2023-01-12 10:21:35 -0300

    correcion tipos

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,13 +1,13 @@
-import { DiaryEntry } from '../types'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../types'
 import diaryData from './diaries.json'
 
 // Asercion de tipos = obligar a TS a considerar que la constante diaryData tiene que funcionar de una forma
 // Es comun al trabajar con APIs externas, uno no controla la creacion de ese objeto.
 // evitar la asercion, a veces es necesario.
-const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry> 
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[] 
 
+export const getEntries = (): DiaryEntry[] => diaries
 
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => diaries
 
-export const getEntries = () => diaries
-export const addEntry = () => null
-
+export const addEntry = (): undefined => undefined
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,15 @@
-export type Weather = 'sunny' | 'rainy' |'cloudy' |'windy' |'stormy'
-export type Visibility = 'great' | 'good' |'ok'|'poor'
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
+export type Visibility = 'great' | 'good' | 'ok' | 'poor'
 
 export interface DiaryEntry {
-  id: number,
-  date: string,
-  weather: Weather,
-  visibility: Visibility,
-  comment: string   
-} 
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
+}
 
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
 
+// evitar crear tipos nuevos y aprovechar lo que existe.//
